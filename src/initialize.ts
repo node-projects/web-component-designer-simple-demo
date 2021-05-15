89fb47c128b2bbeee80754c7e7187d7deb9d1af8
jogibear9988 <jochen.kuehner@gmx.de>
2021-05-15 20:18:37 +0000

    Deploying to gh-pages from @ node-projects/web-component-designer-simple-demo@0ce910f9132c68cf23e2e96a3852905b38fc8c52 🚀

--- src/initialize.ts
+++ src/initialize.ts
@@ -2,7 +2,7 @@ import { DesignerView } from '@node-projects/web-component-designer';
 import serviceContainer from '@node-projects/web-component-designer/dist/elements/services/DefaultServiceBootstrap';
 import '@node-projects/web-component-designer/dist/elements/widgets/designerView/designerView';
 
-async function load() {
+async function load() { //toplevel await does not work with polymer
   await window.customElements.whenDefined("node-projects-designer-view")
   const designerView = <DesignerView><any>document.querySelector("node-projects-designer-view");
   designerView.initialize(serviceContainer);
