db5448de3e5bec1b49761c977348b5c4fc5550a4
Jochen Kühner <jochen.kuehner@gmx.de>
2021-10-23 19:50:55 +0200

    update designer

--- src/initialize.ts
+++ src/initialize.ts
@@ -1,10 +1,11 @@
 import { DesignerView } from '@node-projects/web-component-designer';
-import serviceContainer from '@node-projects/web-component-designer/dist/elements/services/DefaultServiceBootstrap';
+import createDefaultServiceContainer from '@node-projects/web-component-designer/dist/elements/services/DefaultServiceBootstrap';
 import '@node-projects/web-component-designer/dist/elements/widgets/designerView/designerView';
 
 async function load() { //toplevel await does not work with polymer
   await window.customElements.whenDefined("node-projects-designer-view")
   const designerView = <DesignerView><any>document.querySelector("node-projects-designer-view");
+  let serviceContainer = createDefaultServiceContainer();
   designerView.initialize(serviceContainer);
 }
 load();
