4d654a932ccda07c3b352aa7a51494241747647c
Jochen Kühner <jochen.kuehner@gmx.de>
2021-11-21 23:22:11 +0100

    remove use of polymer

--- src/initialize.ts
+++ src/initialize.ts
@@ -2,10 +2,7 @@ import { DesignerView } from '@node-projects/web-component-designer';
 import createDefaultServiceContainer from '@node-projects/web-component-designer/dist/elements/services/DefaultServiceBootstrap';
 import '@node-projects/web-component-designer/dist/elements/widgets/designerView/designerView';
 
-async function load() { //toplevel await does not work with polymer
-  await window.customElements.whenDefined("node-projects-designer-view")
-  const designerView = <DesignerView><any>document.querySelector("node-projects-designer-view");
-  let serviceContainer = createDefaultServiceContainer();
-  designerView.initialize(serviceContainer);
-}
-load();
+await window.customElements.whenDefined("node-projects-designer-view")
+const designerView = <DesignerView><any>document.querySelector("node-projects-designer-view");
+let serviceContainer = createDefaultServiceContainer();
+designerView.initialize(serviceContainer);
