7875fbe4b06af3a91f74460ec7d1d4c0a71c58a6
Jochen Kühner <jochen.kuehner@gmx.de>
2021-11-28 09:06:29 +0100

    update sample

--- src/initialize.ts
+++ src/initialize.ts
@@ -1,8 +1,9 @@
-import { DesignerView } from '@node-projects/web-component-designer';
+import { CursorLinePointerExtensionProvider, DesignerView } from '@node-projects/web-component-designer';
 import createDefaultServiceContainer from '@node-projects/web-component-designer/dist/elements/services/DefaultServiceBootstrap';
 import '@node-projects/web-component-designer/dist/elements/widgets/designerView/designerView';
 
 await window.customElements.whenDefined("node-projects-designer-view")
 const designerView = <DesignerView><any>document.querySelector("node-projects-designer-view");
 let serviceContainer = createDefaultServiceContainer();
+serviceContainer.designerPointerExtensions.push(new CursorLinePointerExtensionProvider());
 designerView.initialize(serviceContainer);
