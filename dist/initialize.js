0b6bae4a285fb87c536ef2308f0ca232445769d9
jogibear9988 <jochen.kuehner@gmx.de>
2021-11-28 08:08:20 +0000

    Deploying to gh-pages from @ node-projects/web-component-designer-simple-demo@7875fbe4b06af3a91f74460ec7d1d4c0a71c58a6 🚀

--- dist/initialize.js
+++ dist/initialize.js
@@ -1,6 +1,8 @@
+import { CursorLinePointerExtensionProvider } from '/web-component-designer-demo/node_modules/@node-projects/web-component-designer/./dist/index.js';
 import createDefaultServiceContainer from '/web-component-designer-demo/node_modules/@node-projects/web-component-designer/dist/elements/services/DefaultServiceBootstrap.js';
 import '/web-component-designer-demo/node_modules/@node-projects/web-component-designer/dist/elements/widgets/designerView/designerView.js';
 await window.customElements.whenDefined("node-projects-designer-view");
 const designerView = document.querySelector("node-projects-designer-view");
 let serviceContainer = createDefaultServiceContainer();
+serviceContainer.designerPointerExtensions.push(new CursorLinePointerExtensionProvider());
 designerView.initialize(serviceContainer);
