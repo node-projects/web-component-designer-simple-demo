63601594e24e57ed0778760cadca555319b1a9eb
jkuehner <jochen.kuehner@kardex.com>
2023-04-29 20:08:23 +0200

    fixes for double load

--- src/initialize.ts
+++ src/initialize.ts
@@ -1,6 +1,4 @@
-import { CursorLinePointerExtensionProvider, DesignerView } from '@node-projects/web-component-designer';
-import createDefaultServiceContainer from '@node-projects/web-component-designer/dist/elements/services/DefaultServiceBootstrap';
-import '@node-projects/web-component-designer/dist/elements/widgets/designerView/designerView';
+import { CursorLinePointerExtensionProvider, DesignerView, createDefaultServiceContainer } from '@node-projects/web-component-designer';
 
 await window.customElements.whenDefined("node-projects-designer-view")
 const designerView = <DesignerView><any>document.querySelector("node-projects-designer-view");
