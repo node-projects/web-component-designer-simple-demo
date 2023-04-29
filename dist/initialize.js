592d6477f7a647816f7b78050f51a0f0cca23fe8
jogibear9988 <jochen.kuehner@gmx.de>
2023-04-29 18:12:58 +0000

    Deploying to gh-pages from @ node-projects/web-component-designer-simple-demo@18d4f89ba44ad3b7c898945ba716840a0a3695e3 🚀

--- dist/initialize.js
+++ dist/initialize.js
@@ -1,4 +1,4 @@
-import { CursorLinePointerExtensionProvider, createDefaultServiceContainer } from '/web-component-designer-demo/node_modules/@node-projects/web-component-designer/./dist/index.js';
+import { CursorLinePointerExtensionProvider, createDefaultServiceContainer } from '@node-projects/web-component-designer';
 await window.customElements.whenDefined("node-projects-designer-view");
 const designerView = document.querySelector("node-projects-designer-view");
 let serviceContainer = createDefaultServiceContainer();
